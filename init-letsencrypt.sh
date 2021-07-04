ce74889f25b0d5ddbebe07355528044e359c8b87
matias <matias.ferron@ing.asutral.edu.ar>
2021-07-04 12:57:41 -0300

    domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(devjjapp.sytes.net)
+domains=(jjdevapp.ddns.net)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="matias.ferron@ing.austral.edu.ar" # Adding a valid address is strongly recommended
